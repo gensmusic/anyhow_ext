54ff2344ca9e045b2414f258b64c00e0fe5b8a61
gensmusic <gensmusic@163.com>
2022-07-07 19:59:27 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,105 @@
+use core::convert::Infallible;
+use std::fmt::Display;
+use std::panic::Location;
+
+/// re-exports
+pub use anyhow::{anyhow, bail, ensure};
+pub use anyhow::{Chain, Error, Ok, Result};
+
+pub trait Context<T, E> {
+    fn context<C>(self, context: C) -> Result<T>
+    where
+        C: Display + Send + Sync + 'static;
+
+    fn with_context<C, F>(self, context: F) -> Result<T>
+    where
+        C: Display + Send + Sync + 'static,
+        F: FnOnce() -> C;
+}
+
+impl<T, E> Context<T, E> for Result<T, E>
+where
+    E: Display,
+    Result<T, E>: anyhow::Context<T, E>,
+{
+    #[inline]
+    #[track_caller]
+    fn context<C>(self, context: C) -> Result<T>
+    where
+        C: Display + Send + Sync + 'static,
+    {
+        let caller = Location::caller();
+        anyhow::Context::context(
+            self,
+            format!(
+                "{} at `{}@{}:{}`",
+                context,
+                caller.file(),
+                caller.line(),
+                caller.column()
+            ),
+        )
+    }
+
+    #[inline]
+    #[track_caller]
+    fn with_context<C, F>(self, context: F) -> Result<T>
+    where
+        C: Display + Send + Sync + 'static,
+        F: FnOnce() -> C,
+    {
+        let caller = Location::caller();
+        anyhow::Context::with_context(self, || {
+            format!(
+                "{} at `{}@{}:{}`",
+                context(),
+                caller.file(),
+                caller.line(),
+                caller.column(),
+            )
+        })
+    }
+}
+
+impl<T> Context<T, Infallible> for Option<T>
+where
+    Option<T>: anyhow::Context<T, Infallible>,
+{
+    #[inline]
+    #[track_caller]
+    fn context<C>(self, context: C) -> Result<T, Error>
+    where
+        C: Display + Send + Sync + 'static,
+    {
+        let caller = Location::caller();
+        anyhow::Context::context(
+            self,
+            format!(
+                "{} at `{}@{}:{}`",
+                context,
+                caller.file(),
+                caller.line(),
+                caller.column()
+            ),
+        )
+    }
+
+    #[inline]
+    #[track_caller]
+    fn with_context<C, F>(self, context: F) -> Result<T, Error>
+    where
+        C: Display + Send + Sync + 'static,
+        F: FnOnce() -> C,
+    {
+        let caller = Location::caller();
+        anyhow::Context::with_context(self, || {
+            format!(
+                "{} at `{}@{}:{}`",
+                context(),
+                caller.file(),
+                caller.line(),
+                caller.column(),
+            )
+        })
+    }
+}
